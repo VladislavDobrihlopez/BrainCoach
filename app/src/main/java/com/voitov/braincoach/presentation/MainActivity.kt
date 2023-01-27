d422dea0ef39d01d9549a634df1db113fbbe23c3
Vladislav Voitov <dobrihlopez@gmail.com>
2023-01-27 22:41:38 +0300

    add jetpack navigation instead of using supportFragmentManager
    Signed-off-by: Vladislav Voitov <dobrihlopez@gmail.com>

--- app/src/main/java/com/voitov/braincoach/presentation/MainActivity.kt
+++ app/src/main/java/com/voitov/braincoach/presentation/MainActivity.kt
@@ -1,28 +1,12 @@
 package com.voitov.braincoach.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.voitov.braincoach.R
-import com.voitov.braincoach.data.GameRepositoryImpl
-import com.voitov.braincoach.domain.entity.LevelSettings
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val q = GameRepositoryImpl.generateQuestionUseCase(LevelSettings(10, 0, 5, 50, 3, 10))
-        Log.d("MainActivity", q.toString())
-
-        if (savedInstanceState == null) {
-            launchHelloFragment()
-        }
-    }
-
-    private fun launchHelloFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerMain, HelloFragment.newInstance())
-            .commit()
     }
 }
